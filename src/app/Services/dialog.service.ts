ad0b533d832cd0f04f8ab5609cae01a0ad65432e
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 09:10:16 +0100

    Add dialog-service

---
+++ src/app/Services/dialog.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { DialogMode } from '../Protocol/dialog-mode.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+
+  constructor() { }
+
+  width_Medium = '500px';
+
+  getNewOrEdit(mode: DialogMode) {
+    if (mode == DialogMode.New) {
+      return 'New';
+    }
+    else if (mode == DialogMode.Edit) {
+      return 'Edit';
+    }
+    else {
+      return '';
+    }
+  }
+}
