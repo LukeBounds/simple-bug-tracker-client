25f4fb05a2ef7fe459974907a2fb26bc1e10baa0
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 10:17:49 +0100

    Add bug/bugs to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { UsersComponent } from './Components/users/users/users.component';
+import { BugsComponent } from './Components/bugs/bugs/bugs.component';
+import { BugComponent } from './Components/bugs/bug/bug.component';
 
 export const routes: Routes = [
     {
         path: 'users',
         component: UsersComponent,
+    },
+    {
+        path: 'bugs',
+        component: BugsComponent,
+    },
+    {
+        path: 'bug/:id',
+        component: BugComponent,
     },
 ];
