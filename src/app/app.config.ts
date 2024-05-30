611cf9e5850a8c414270352cfca4600d67b6d894
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 09:11:49 +0100

    provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 import { configFactory } from './Factories/config.factory';
 import { ConfigService } from './Services/config.service';
 
@@ -10,6 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
     {
       provide: APP_INITIALIZER,
       useFactory: configFactory,
